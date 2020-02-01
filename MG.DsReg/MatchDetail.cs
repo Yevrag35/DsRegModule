e8d876db2e7e8eb9598d347757ec849103ae21d6
Mike Garvey <mike.garvey@yevrag35.com>
2020-02-01 17:09:07 -0500

    to 4.5.2

--- MG.DsReg/MatchDetail.cs
+++ MG.DsReg/MatchDetail.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Reflection;
 
 namespace MG.DsReg
@@ -30,39 +30,6 @@ namespace MG.DsReg
 
             this.Value = this.ConvertTo(value.Trim());
         }
-
-        //public void SetValue(object newObject)
-        //{
-
-        //    if (this.Property.PropertyType.Equals(typeof(bool?)))
-        //    {
-        //        if (this.Value.Contains("YES"))
-        //            this.Property.SetValue(newObject, true);
-
-        //        else if (this.Value.Contains("NO"))
-        //            this.Property.SetValue(newObject, false);
-        //    }
-        //    if (this.Property.PropertyType.Equals(typeof(int?)) && int.TryParse(this.Value, out int number))
-        //    {
-        //        this.Property.SetValue(newObject, number);
-        //    }
-        //    else if (this.Property.PropertyType.Equals(typeof(Guid?)) && Guid.TryParse(this.Value, out Guid guid))
-        //    {
-        //        this.Property.SetValue(newObject, guid);
-        //    }
-        //    else if (this.Property.PropertyType.Equals(typeof(DateTime?)) && DateTime.TryParse(this.Value, out DateTime dt))
-        //    {
-        //        this.Property.SetValue(newObject, dt);
-        //    }
-        //    else if (this.Property.PropertyType.Equals(typeof(Version)) && Version.TryParse(this.Value, out Version vers))
-        //    {
-        //        this.Property.SetValue(newObject, vers);
-        //    }
-        //    else if (this.Property.PropertyType.Equals(typeof(string)) && !string.IsNullOrEmpty(this.Value))
-        //    {
-        //        this.Property.SetValue(newObject, this.Value);
-        //    }
-        //}
 
         private object ConvertTo(string s)
         {
